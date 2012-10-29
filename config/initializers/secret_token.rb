4eafc3778b0f46f733f36baf6566d948e4c40c0a
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2012-10-29 21:04:41 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuppliersRating::Application.config.secret_token = 'cc7d04bd0ad1f5b98063f52d49d3ba7e186c6e14780aad513aa1e5036803f1f508094f3c18e6fd9e9b7eee3b10caf55f85d7260d9c0677068207eee71116810c'
