f0ce7db80f299384d46bd2ef57cef05932e7378d
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2012-10-29 21:31:53 +0200

    setup test environment

---
+++ spec/spec_helper.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
+
+Spork.prefork do
+  # This file is copied to spec/ when you run 'rails generate rspec:install'
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+  require "capybara/rspec"
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  Dir[Rails.root.join("spec/shared_behaviors/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+
+    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+    config.use_transactional_fixtures = true
+
+    config.infer_base_class_for_anonymous_controllers = false
+
+    config.order = "random"
+
+    config.include FactoryGirl::Syntax::Methods
+    config.include Devise::TestHelpers, type: :controller
+  end
+end
+
+Spork.each_run do
+  FactoryGirl.reload
+end
