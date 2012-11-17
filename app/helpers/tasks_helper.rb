4a2d73e305bad6608d8684846c0171a66512643f
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2012-11-17 17:47:10 +0200

    task show page

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,4 +2,8 @@ module TasksHelper
   def task_supplier_and_service(task)
     "#{task.supplier_name} [#{task.service_name}]"
   end
+
+  def task_full_title(task)
+    [task.title, task.supplier_name, task.service_name, number_to_currency(task.cost, unit: t(:lei))].compact.join(" | ")
+  end
 end
