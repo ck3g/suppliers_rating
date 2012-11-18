3093441decdb3c82117d17f0502d4ede62d43f48
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2012-11-18 16:23:07 +0200

    fix chain commenting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,9 +8,11 @@ class CommentsController < ApplicationController
   def create
     @comment = @task.comments.new params[:comment]
     @comments = @task.comments.order(:created_at)
-    @comment.save
-    if params[:close] && params[:task_rating]
-      @task.close_with_rating! params[:task_rating].to_i
+    if @comment.save
+      if params[:close] && params[:task_rating]
+        @task.close_with_rating! params[:task_rating].to_i
+      end
+      @comment = @task.comments.new message: ""
     end
     render 'tasks/reload'
   end
